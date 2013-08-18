0cfd3bf00090e04d610016804241905f453754ec
Kev <Kev@unknown-00:23:6c:95:07:65.home>
2013-08-18 19:10:13 +0100

    game is now set up to display basic state from json file, models and views are set up as objects with prototypes.

--- build/js/main.js
+++ build/js/main.js
@@ -1,21 +1,113 @@
-$(document).ready( function(){
-    moveContainerTopToCentre();
-    $(document).keypress(onKeyPressed);
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+var GameModel = function() {
+    var instance = GameModel.prototype;
+    console.log("New game initialised");
+    
+    $.getJSON("data/levelData.json", function(data){
+        instance.levelData = data.levels;
+        instance.currentLevel = new LevelModel(instance.levelData[0]);
+        instance.view = new GameView(instance);
+        instance.view.level.initialise();
+        instance.view.loop();
+    });
+}
+
+var GameView = function(gameModel) {
+    var instance = GameView.prototype;
+    
+    instance.model = gameModel;
+    
+    instance.initialise = function() {
+        console.log("Initialising game view");
+        
+        var canvas = $('.gameCanvas')[0];
+        instance.height = canvas.height;
+        instance.width = canvas.width;
+        instance.context = canvas.getContext('2d');
+        this.level = new LevelView(this.model.currentLevel);
+    }
+    
+    instance.loop = function() { 
+        window.requestAnimFrame($.proxy(this.loop, this));
+        this.context.clearRect(0, 0, this.height, this.width);
+        this.render();
+    }
+    
+    instance.render = function() {
+        this.level.render();
+        //this.player.render();
+    }
+    
+    instance.initialise();
+}
+
+var LevelModel = function(levelData) {
+    var instance = LevelModel.prototype;
+    instance.initialise = function(levelData) {
+        this.switches = levelData.switches;
+        this.doors = levelData.doors;
+    }
+    
+    instance.initialise(levelData);
+}
+
+var LevelView = function(model) {
+    var instance = LevelView.prototype;
+    instance.model = model;
+    
+    instance.initialise = function() {
+        this.doorPositions = [];
+        this.switchPositions = [];
+        for (var i = 0; i < this.model.doors.length; i++) {
+            this.doorPositions.push(mapValue(i+1, 0, this.model.doors.length+1, 0, game.view.width));
+        }
+        for (var i = 0; i < this.model.switches.length; i++) {
+            this.switchPositions.push(this.doorPositions[this.model.switches[i].position] - 50);
+        }
+    }
+    
+    instance.render = function() {
+        var ctx = game.view.context;
+        ctx.fillStyle = "#111111";
+        
+        for (var i = 0; i < this.model.doors.length; i++) {
+            var currentDoor = this.model.doors[i];
+            if(currentDoor.position == "closed"){
+                ctx.fillRect(this.doorPositions[i], 0, 10, game.view.height);
+            } else {
+                ctx.fillRect(this.doorPositions[i], 0, 10, 10);
+                ctx.fillRect(this.doorPositions[i], game.view.height - 10, 10, 10);
+            }
+        }
+        for (var i = 0; i < this.model.switches.length; i++) {
+            var currentSwitch = this.model.switches[i];
+            
+            ctx.beginPath();
+            ctx.arc(this.switchPositions[i], game.view.height/2, 10, 0, 2*Math.PI);
+            ctx.fill();
+        }
+    }
+    
+}
+
+var PlayerView = function() {
+}
+
+$(document).ready(function() {
+    window.game = new GameModel();
 });
 
-var moveContainerTopToCentre = function(){
-    $('.container').css({top: "-1000px"});
-    moveContainerToCentre();
-}
-                                                               
-var moveContainerToCentre = function(){
-    $('.container').animate({top: "0px"}, 1000);
-}
-
-var animateToNextLevel = function() {
-    $('.container').animate({top: "1000px"}, 1000, moveContainerTopToCentre);
-}
-
-var onKeyPressed = function(event){
-    animateToNextLevel();
+function mapValue(value, low1, high1, low2, high2) {
+    var range1 = high1 - low1;
+    var range2 = high2 - low2;
+    return ((value - low1) / range1 * range2 + low2);
 }
