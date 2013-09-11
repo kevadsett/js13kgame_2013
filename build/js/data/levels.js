50d9765f953fabb86198bb1e6f796eacfc0d3873
Kev Adsett <kev.adsett@kineo.com>
2013-09-11 14:48:35 +0100

    Added new levels

--- build/js/data/levels.js
+++ build/js/data/levels.js
@@ -37,6 +37,34 @@ LateRunner.LevelData = {
                 { state: "closed" },
                 { state: "open" }
             ]
+        },
+        {
+            switches:[ 
+                { connectedDoors: [0, 2], doorPosition: 0 },
+                { connectedDoors: [0], doorPosition: 1 },
+                { connectedDoors: [0, 1], doorPosition: 1 },
+                { connectedDoors: [0], doorPosition: 2 }
+            ],
+            doors:[
+                { state: "closed" },
+                { state: "closed" },
+                { state: "open" }
+            ]
+        },
+        {
+            switches:[ 
+                { connectedDoors: [0, 1], doorPosition: 0 },
+                { connectedDoors: [1], doorPosition: 1 },
+                { connectedDoors: [0, 3], doorPosition: 1 },
+                { connectedDoors: [2], doorPosition: 2 },
+                { connectedDoors: [3], doorPosition: 3 }
+            ],
+            doors:[
+                { state: "open" },
+                { state: "closed" },
+                { state: "open" },
+                { state: "open" }
+            ]
         }
     ]
 }
